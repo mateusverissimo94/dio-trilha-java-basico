0e14a05956bbd0c7a22674919d9acc24f9e66b2e
Mateus Barros <mateusverissimo94@gmail.com>
2023-06-15 16:28:47 -0300

    Condicionais

--- EstruturasCondicionais/src/ResultadoEscolar.java
+++ EstruturasCondicionais/src/ResultadoEscolar.java
@@ -1,12 +1,20 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 6;
+        // int nota = 6;
 
-        if (nota >= 7)
-            System.out.println("Aprovado");
+        // if (nota >= 7)
+        // System.out.println("Aprovado");
+        // else if (nota >= 5 && nota < 7)
+        // System.out.println("Recuperação");
+        // else
+        // System.out.println("Reprovado");
 
-        else
-            System.out.println("Reprovado");
+        int nota = 7;
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+
+        String resultado2 = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
 
     }
 }
